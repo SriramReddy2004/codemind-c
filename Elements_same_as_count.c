07d2e8cbbff962f6733a9bb79dc8e76d6030583f
SriramReddy2004 <119839562+SriramReddy2004@users.noreply.github.com>
2022-12-13 20:16:34 +0530

    #include<stdio.h>
    int main(){
        
        int j,i,n,c,p=0;
        scanf("%d",&n);
        int a[n];
        for(i=0;i<n;i++) scanf("%d",&a[i]);
        for(i=0;i<n;i++){
            c=0;
            for(j=0;j<n;j++){
                if(a[i]==a[j]){
                    c++;
                    if(j!=i) a[j]=0;
                }
            }
            if(c==a[i]){
                printf("%d ",a[i]);
                p++;
            }
        }
        if(p==0) printf("-1");
        
        return 0;
    }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    
+    int j,i,n,c,p=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=0;i<n;i++){
+        c=0;
+        for(j=0;j<n;j++){
+            if(a[i]==a[j]){
+                c++;
+                if(j!=i) a[j]=0;
+            }
+        }
+        if(c==a[i]){
+            printf("%d ",a[i]);
+            p++;
+        }
+    }
+    if(p==0) printf("-1");
+    
+    return 0;
+}
