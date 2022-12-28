872d5d2f49364bfba59a213b6bcb508040640d62
SriramReddy2004 <119839562+SriramReddy2004@users.noreply.github.com>
2022-12-28 11:27:49 +0530

    #include<stdio.h>
    int main(){
    
        int n,i,j,k,a,b,t=0;
        scanf("%d",&n);
        int arr[n];
        for(i=0;i<n;i++) scanf("%d",&arr[i]);
        scanf("%d %d",&a,&b);
        if(a>b){
            a*=b;
            b=a/b;
            a/=b;
        }
        for(i=0;i<n;i++){
            if(arr[i]>=a && arr[i]<=b) continue;
            else{
                printf("%d ",arr[i]);
                t++;
            }
        }
        if(t==0) printf("-1");
        return 0;
    }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int n,i,j,k,a,b,t=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++) scanf("%d",&arr[i]);
+    scanf("%d %d",&a,&b);
+    if(a>b){
+        a*=b;
+        b=a/b;
+        a/=b;
+    }
+    for(i=0;i<n;i++){
+        if(arr[i]>=a && arr[i]<=b) continue;
+        else{
+            printf("%d ",arr[i]);
+            t++;
+        }
+    }
+    if(t==0) printf("-1");
+    return 0;
+}
